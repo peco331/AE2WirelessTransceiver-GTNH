221e6491456ce1aa446b6ce527779e05942cdca2
Local Build <local@build>
2026-08-14 15:01:11 +0800

    Scaffold: AE2 Wireless Transceiver GTNH mod (build files + minimal mod class)

---
+++ src/main/java/cn/gtnh/ae2wtx/AE2Wtx.java
@@ -0,0 +1,41 @@
+package cn.gtnh.ae2wtx;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(
+    modid = AE2Wtx.MODID,
+    version = Tags.VERSION,
+    name = AE2Wtx.NAME,
+    acceptedMinecraftVersions = "[1.7.10]",
+    dependencies = "required-after:appliedenergistics2;required-after:gregtech")
+public class AE2Wtx {
+
+    public static final String MODID = "ae2wtx";
+    public static final String NAME = "AE2 Wireless Transceiver (GTNH)";
+    public static final Logger LOG = LogManager.getLogger(MODID);
+
+    @SidedProxy(clientSide = "cn.gtnh.ae2wtx.ClientProxy", serverSide = "cn.gtnh.ae2wtx.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+}
